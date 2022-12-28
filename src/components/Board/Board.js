26b46e7c0db747fa150396ff1343167dd980c695
Jeronimo luis Vera <jeronimo.l.vera@exxonmobil.com>
2022-12-28 11:08:34 -0300

    2

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,18 +1,20 @@
-import React from 'react'
+import React from 'react';
 import './Board.css';
+import Tile from '../Tile';
 
 const Board = (props) => {
+    const gridConfig = {
+        gridTemplateColumns: `repeat(${Math.sqrt(props.numTiles)}, 1fr)`,
+        gridTemplateRows: `repeat(${Math.sqrt(props.numTiles)}, 1fr)`,
+    };
 
-  const gridConfig = {
-    gridTemplateColumns: `repeat(${Math.sqrt(props.numTiles)}, 1fr)`,
-    gridTemplateRows: `repeat(${Math.sqrt(props.numTiles)}, 1fr)`,
-  }
+    const tiles = props.tiles.map((tile) => <Tile {...tile} />);
 
+    return (
+        <div className='Board' style={gridConfig}>
+            {tiles}
+        </div>
+    );
+};
 
-  return (
-    <div className='Board' style={gridConfig}>
-    </div>
-  )
-}
-
-export default Board
+export default Board;
--- src/components/Tile/Tile.js
+++ src/components/Tile/Tile.js
@@ -1,13 +1,17 @@
-import React from 'react'
+import React from 'react';
 
-import './Tile.css'
+import './Tile.css';
 
-const Tile = () => {
+const Tile = (props) => {
+    const dynamicColor =
+        props.selected || props.matched
+            ? { backgroundColor: props.color }
+            : null;
+    return (
+        <div className='Tile' style={dynamicColor}>
+            {props.selected || props.matched ? <props.svg /> : null}
+        </div>
+    );
+};
 
-  return (
-    <div className='Tile'>
-    </div>
-  )
-}
-
-export default Tile
+export default Tile;
